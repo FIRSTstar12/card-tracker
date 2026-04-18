b0424114f1cf75882475d268abcc52bbaa42a306
Stella <24murphys@gmail.com>
2026-04-18 13:45:24 -0600

    xxx

--- main.py
+++ main.py
@@ -1,30 +1,81 @@
 import json
+import os
+
 account = "userData.json"
+user = ""
 def newAccount():
+    print("NEW ACCOUNT CREATION")
     username = input("Enter in your username: ")
     password = input("Enter in your password: ")
 
+    try:
+        with open(account, "r") as file:
+            data = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        data = {}
+    
+    if username in data:
+        print("Username already exists.")
+        return
+
+    data[username] = password
+    makeNewFile(username)
+
+    with open(account, "w") as file:
+        json.dump(data, file, indent=4)
+
+    print("Account created successfully.")
+
+def delAccount():
+    print("ACCOUNT DELETION")
+    username = input("Enter in your username: ")
+
+    try:
+        with open(account, "r") as file:
+            data = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        data = {}
+    
+    if username in data:
+        delete = input("Are you sure you want to delete this account " + username + " (y/n): ")
+        if(delete.lower() == 'y'):
+            del data[username]
+            filename = username + "_card_collection.json"
+            os.remove()
+            print(username + " has been deleted")
+            
+            with open(account, "w") as file:
+                json.dump(data, file, indent=4)
+
+        else:
+            print(username + " will not be deleted")
+    else:
+        print(username + " not found")
+
 def login():
+    print("Welcome to the card tracker.\n")
     username = input("Enter your username: ")
     with open(account, "r") as file:
         data = json.load(file)
-        for key, value in data.items():
-            if(key == username):
-                password = input("Please enter your password: ")
-                if(value == password):
-                    return True
-            else:
-                print(username + " is not in our database.")
-                return False
+        if username not in data:
+            print(username + " is not in our database.")
+            return False
+
+        password = input("Please enter your password: ")
+        if data[username] == password:
+            return True
+        else:
+            print("Invalid password")
+            return False
 
 def addToFile(filename):
     try:
         with open(filename, "r") as file:
             userData = json.load(file)
         if not isinstance(userData, list):
-            userData = []
+            userData = {}
     except (FileNotFoundError, json.JSONDecodeError):
-        userData = []
+        userData = {}
 
     while True:
         card = input("Add card name (or type 'done'): ")
@@ -34,14 +85,23 @@ def addToFile(filename):
 
     return userData
 
-def makeNewFile():
-    name = input("Please enter your name: ")
+def makeNewFile(name):
     filename = name + "_card_collection.json"
     startingData = addToFile(filename)
     try:
         with open(filename, "w") as file:
-            # indent=4 makes the file human-readable
             json.dump(startingData, file, indent=4)
-        print(f"Success! '{name}' has been created.")
+        print(f"Success! '{filename}' has been created.")
     except IOError as e:
         print(f"An error occurred while creating the file: {e}")
+
+if(login() == False):
+    makeAccount = input("Do you want to make a new account?(y/n): ")
+    if(makeAccount.lower() == "y"):
+        newAccount()
+    else:
+        print("Bye!")
+else:
+    print("Welcome")
+
+delAccount()
